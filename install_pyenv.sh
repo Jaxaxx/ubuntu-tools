087e859f4ab75a2f598193b70eeb35f656ded27a
李明 <jax0910@163.com>
2023-09-25 20:38:20 +0800

    add pyenv.sh

---
+++ install_pyenv.sh
@@ -0,0 +1,14 @@
+# 
+# sudo curl https://pyenv.run | bash
+
+
+## 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+source  ~/.bashrc
+
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
